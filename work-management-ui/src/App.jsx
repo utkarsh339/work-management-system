f0cbdbce8d920c4a9c5aa105d0962baa578d82bb
Utkarsh Rai <raiutkarsh33@gmail.com>
2026-01-11 10:35:52 +0530

    Add Routing with Login and Dashboard Pages

--- work-management-ui/src/App.jsx
+++ work-management-ui/src/App.jsx
@@ -1,9 +1,17 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
 
 function App() {
   return (
     <div>
-      <Login />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="*" element={<Navigate to="/login" />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ work-management-ui/src/pages/Dashboard.jsx
@@ -0,0 +1,10 @@
+function Dashboard() {
+  return (
+    <>
+      <h2>Dashboard</h2>
+      <p>Welcome to Work Management System. You are in Dashboard Page.</p>
+    </>
+  );
+}
+
+export default Dashboard;
