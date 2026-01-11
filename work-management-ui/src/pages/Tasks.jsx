0d25370a064fa4b9443a0636328c9e476e8805e7
Utkarsh Rai <raiutkarsh33@gmail.com>
2026-01-11 21:13:44 +0530

    Updated tasks page with mock table data

--- work-management-ui/src/pages/Tasks.jsx
+++ work-management-ui/src/pages/Tasks.jsx
@@ -1,10 +1,75 @@
+const mockTasks = [
+  {
+    id: 1,
+    title: "Prepare Monthly Report",
+    assignedBy: "Manager",
+    status: "Pending",
+    dueDate: "2026-01-20",
+  },
+  {
+    id: 2,
+    title: "Fix login bug",
+    assignedBy: "Tech Lead",
+    status: "In Progress",
+    dueDate: "2026-01-12",
+  },
+  {
+    id: 3,
+    title: "Update documentation",
+    assignedBy: "Admin",
+    status: "Completed",
+    dueDate: "2026-01-10",
+  },
+];
+
 function Tasks() {
   return (
     <>
       <h2>My Tasks</h2>
-      <p>This page will show tasks assigned to the user.</p>
+      <p>Tasks Assigned to You.</p>
+
+      <table style={styles.table}>
+        <thead>
+          <tr>
+            <th style={styles.th}>Title</th>
+            <th style={styles.th}>Assigned By</th>
+            <th style={styles.th}>Status</th>
+            <th style={styles.th}>Due Date</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {mockTasks.map((tasks) => (
+            <tr key={tasks.id}>
+              <td style={styles.td}>{tasks.title}</td>
+              <td style={styles.td}>{tasks.assignedBy}</td>
+              <td style={styles.td}>{tasks.status}</td>
+              <td style={styles.td}>{tasks.dueDate}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 }
+
+const styles = {
+  table: {
+    width: "100%",
+    borderCollapse: "collapse",
+    marginTop: "16px",
+    backgroundColor: "#fff",
+  },
+  th: {
+    border: "1px solid #e5e7eb",
+    padding: "8px",
+    textAlign: "center",
+    backgroundColor: "#f9fafb",
+  },
+  td: {
+    border: "1px solid #e5e7eb",
+    padding: "8px",
+  },
+};
 
 export default Tasks;
