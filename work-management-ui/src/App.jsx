bf8d46aace8541d15ec78563c9e2f3e53dfefc7a
utkarsh <raiutkarsh33@gmail.com>
2026-01-23 12:27:26 +0530

    Migrate authentication state to Redux Toolkit hooks

--- work-management-ui/src/App.jsx
+++ work-management-ui/src/App.jsx
@@ -1,39 +1,28 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { useState } from "react";
 import Login from "./pages/Login.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
 import Tasks from "./pages/Tasks.jsx";
 import Approvals from "./pages/Approvals.jsx";
 import ProtectedRoute from "./routes/ProtectedRoute.jsx";
+import { useSelector, useDispatch } from "react-redux";
+import { login, logout } from "./store/authSlice.js";
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(
-    localStorage.getItem("isAuthenticated") === "true"
-  );
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  const dispatch = useDispatch();
   return (
     <div>
       <BrowserRouter>
         <Routes>
           <Route
             path="/login"
-            element={
-              <Login
-                onLogin={() => {
-                  setIsAuthenticated(true);
-                  localStorage.setItem("isAuthenticated", "true");
-                }}
-              />
-            }
+            element={<Login onLogin={() => dispatch(login())} />}
           />
           <Route
             element={
               <ProtectedRoute
                 isAuthenticated={isAuthenticated}
-                onLogout={() => {
-                  setIsAuthenticated(false);
-                  localStorage.removeItem("isAuthenticated");
-                  localStorage.removeItem("tasks");
-                }}
+                onLogout={() => dispatch(logout())}
               />
             }
           >
