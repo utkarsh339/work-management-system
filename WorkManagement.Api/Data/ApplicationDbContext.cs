55d79b71d93a225d0eaa7b5027b240ceaadf5c47
utkarsh <raiutkarsh33@gmail.com>
2026-01-25 12:44:35 +0530

    Configure EF Core with SQL Server and DbContext

---
+++ WorkManagement.Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WorkManagement.Api.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
